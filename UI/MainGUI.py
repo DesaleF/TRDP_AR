1c8fa2bf9fb67c4659207c30c5ec198e601bbddf
Desale Fentaw <desale.df@gmail.com>
2020-12-02 17:27:38 +0100

    brush size and eraser sizes are added to the UI

--- UI/MainGUI.py
+++ UI/MainGUI.py
@@ -91,8 +91,10 @@ class App(QMainWindow):
         """  Create and add menu items into menu bar
         """
         # list of menu bars
-        self.fileMenuBar = self.menuBar().addMenu("File")
-        self.editMenuBar = self.menuBar().addMenu("Edit")
+        self.fileMenuBar = self.menuBar().addMenu("&File")
+        self.editMenuBar = self.menuBar().addMenu("&Edit")
+        self.brushSizeBar = self.menuBar().addMenu("&Pencil Size")
+        self.eraserSizeBar = self.menuBar().addMenu("&Eraser Size")
 
         # file menu Bar components
         self.startVideoButton = self.fileMenuBar.addAction("Strat Video")
@@ -141,6 +143,40 @@ class App(QMainWindow):
         self.actionExit.setShortcut('Ctrl+Q')
         self.actionExit.setIcon(QIcon("./assets/icons/icons8-exit-50.png"))
         self.actionExit.triggered.connect(self.exitApp)
+
+        # Pencil size menu
+        self.threepxActionSize = self.brushSizeBar.addAction("3px")
+        self.threepxActionSize.triggered.connect(self.threePixel)
+        self.threepxActionSize.setIcon(QIcon("./assets/icons/penSize/px3.png"))
+
+        self.fivepxActionSize = self.brushSizeBar.addAction("5px")
+        self.fivepxActionSize.triggered.connect(self.fivePixel)
+        self.fivepxActionSize.setIcon(QIcon("./assets/icons/penSize/px5.png"))
+
+        self.sevenpxActionSize = self.brushSizeBar.addAction("7px")
+        self.sevenpxActionSize.triggered.connect(self.sevenPixel)
+        self.sevenpxActionSize.setIcon(QIcon("./assets/icons/penSize/px7.png"))
+
+        self.ninepxActionSize = self.brushSizeBar.addAction("9px")
+        self.ninepxActionSize.triggered.connect(self.ninePixel)
+        self.ninepxActionSize.setIcon(QIcon("./assets/icons/penSize/px9.png"))
+
+        # Eraser size menu
+        self.erase10ActionSize = self.eraserSizeBar.addAction("10px")
+        self.erase10ActionSize.triggered.connect(self.eraserSize10)
+        self.erase10ActionSize.setIcon(QIcon("./assets/icons/penSize/px3.png"))
+
+        self.erase20ActionSize = self.eraserSizeBar.addAction("20px")
+        self.erase20ActionSize.triggered.connect(self.eraserSize20)
+        self.erase20ActionSize.setIcon(QIcon("./assets/icons/penSize/px5.png"))
+
+        self.erase30ActionSize = self.eraserSizeBar.addAction("30px")
+        self.erase30ActionSize.triggered.connect(self.eraserSize30)
+        self.erase30ActionSize.setIcon(QIcon("./assets/icons/penSize/px7.png"))
+
+        self.erase40ActionSize = self.eraserSizeBar.addAction("40px")
+        self.erase40ActionSize.triggered.connect(self.eraserSize40)
+        self.erase40ActionSize.setIcon(QIcon("./assets/icons/penSize/px9.png"))
 
     def finalUi(self):
         """
@@ -220,6 +256,32 @@ class App(QMainWindow):
             self.erase_flag = True
             QApplication.setOverrideCursor(self.eraseCursor)
             self.drawing = False
+
+    # pencil size
+    def threePixel(self):
+        self.brushSize = 3
+
+    def fivePixel(self):
+        self.brushSize = 5
+
+    def sevenPixel(self):
+        self.brushSize = 7
+
+    def ninePixel(self):
+        self.brushSize = 9
+
+    # Eraser Size
+    def eraserSize10(self):
+        self.clear_size = 10
+
+    def eraserSize20(self):
+        self.clear_size = 20
+
+    def eraserSize30(self):
+        self.clear_size = 30
+
+    def eraserSize40(self):
+        self.clear_size = 40
 
     def openFileNamesDialog(self):
         options = QtWidgets.QFileDialog.Options()
